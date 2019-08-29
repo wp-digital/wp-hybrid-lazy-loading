2449e1d9fd168dca10803fb978b08dc06cd587e2
Viktor Kuliebiakin <kuliebiakin@gmail.com>
2019-08-29 15:10:51 +0300

    add plugin homepage

--- wp-hybrid-lazy-loading.php
+++ wp-hybrid-lazy-loading.php
@@ -2,7 +2,8 @@
 /**
  * Plugin Name: Hybrid Lazy Loading
  * Description: A Progressive Migration To Native Lazy Loading.
- * Version: 1.0.0
+ * Plugin URI: https://github.com/innocode-digital/wp-hybrid-lazy-loading
+ * Version: 1.0.1
  * Author: Innocode
  * Author URI: https://innocode.com
  * Tested up to: 5.2.2
@@ -10,7 +11,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-define( 'INNOCODE_WP_HYBRID_LAZY_LOADING_VERSION', '1.0.0' );
+define( 'INNOCODE_WP_HYBRID_LAZY_LOADING_VERSION', '1.0.1' );
 define( 'INNOCODE_WP_HYBRID_LAZY_LOADING_FILE', __FILE__ );
 
 require_once __DIR__ . '/src/Assets.php';
